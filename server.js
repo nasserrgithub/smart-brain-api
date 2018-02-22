66dc4580e6879b4e5c9da1d27c34109a9e1498de
nasserrgithub <nasserr.corcuera@gmail.com>
2018-02-22 15:41:15 +0800

    adding smart-brain-api

---
+++ server.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', //this is equal to your localhost
+    user : 'postgres',
+    password : '22JuLy20',
+    database : 'smartbrain'
+  }
+});
+
+/*db.select('*').from('users').then(data => {
+	console.log(data);
+});*/
+ 
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/*const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+
+		{
+			id: '124',
+			name: 'Nass',
+			email: 'nass@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gmail.com'
+		}
+	]
+}*/
+
+/*app.get('/', (req, res) => {
+	db.select('*').from('users').then(users => res.send(JSON.stringify(users)));
+})*/
+
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
+// could be app.post('/signin', (req, res) => signin.handleSignin(db, bcrypt)); , look at signin.js for changes
+
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt)); //dependency injection
+
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
+
+app.put('/image', (req, res) => image.handleImage(req, res, db));
+
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
+ 
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
+
+
+/*
+/ -> res = this is working
+/signin -> POST = success/fail
+/register -> POST = user
+/profile/:userId -> GET = user
+/image -> PUT = user (for the rank)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
